329225314083b69b1e99c0225f35fce50a9a25d5
vraj patel <86962138+Vrajp08@users.noreply.github.com>
2022-07-15 14:58:54 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./Footer.css";
 
-
 const Footer = () => {
   return (
     <div className="footer">
       <span className="name">
-        Inshorts clone made by -{" "}
+        Inshorts clone 
       </span>
       <hr style={{ width: "90%" }} />
-      <div className="iconContainer">       
-        <i class="fa-brands fa-instagram fa-2x"></i>    
-          <i className="fab fa-linkedin fa-2x"></i
+      <div className="iconContainer">
+        <i class="fa-brands fa-instagram fa-2x"></i>
+          <i className="fab fa-linkedin fa-2x"></i>
       </div>
     </div>
   );
